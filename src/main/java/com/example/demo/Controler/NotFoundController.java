ad18223fd96dd68db43293a1f479eeb25961d397
NgocLong18 <51955577+NgocLong18@users.noreply.github.com>
2020-11-02 11:25:07 +0700

    dùng 404 thì lỗi full css, đang khắc phục

--- src/main/java/com/example/demo/Controler/NotFoundController.java
+++ src/main/java/com/example/demo/Controler/NotFoundController.java
@@ -1,14 +1,12 @@
 package com.example.demo.Controler;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NotFoundController {
     @GetMapping("**")
     public String errorNotFound(){
-        return "notfound";
+        return "404notfound";
     }
 }
